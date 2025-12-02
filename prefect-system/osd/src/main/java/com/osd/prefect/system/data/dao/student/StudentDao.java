093164c4b8483b927e456c864dc23fd299e8a54e
leeaneee <leeaneglazel.reyes@gmail.com>
2025-12-02 13:27:06 +0800

    Updated StudentDao

--- prefect-system/osd/src/main/java/com/osd/prefect/system/data/dao/student/StudentDao.java
+++ prefect-system/osd/src/main/java/com/osd/prefect/system/data/dao/student/StudentDao.java
@@ -2,7 +2,9 @@ package com.osd.prefect.system.data.dao.student;
 
 import com.osd.prefect.system.model.student.Student;
 
+import java.util.List;
+
 public interface StudentDao {
     Student getStudentByID(String studentID);
-
+    List<Student> getAllStudentsInDepartment(String departmentID);
 }
