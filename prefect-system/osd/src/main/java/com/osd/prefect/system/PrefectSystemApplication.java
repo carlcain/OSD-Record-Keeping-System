c96bee5117a6b7c8f43bb95fe5004e919ad6a497
G33oo <geoffreyderojas@gmail.com>
2025-12-04 15:41:20 +0700

    kulang pa appeal, sensya na tao lang

--- prefect-system/osd/src/main/java/com/osd/prefect/system/PrefectSystemApplication.java
+++ prefect-system/osd/src/main/java/com/osd/prefect/system/PrefectSystemApplication.java
@@ -1,4 +1,28 @@
 package com.osd.prefect.system;
 
+import java.util.Scanner;
+
+
+
 public class PrefectSystemApplication {
-}
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+            System.out.println("Choose an action: ");
+            System.out.println("1 - Appeal  2 - Logout");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+
+        }
+
+
+
+            }
+
+        }
+
